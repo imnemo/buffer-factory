f7c434ab8ef5c4ebacc01ef04661487f3b46781a
imnemo <291400733@qq.com>
2020-06-08 11:14:42 +0800

    unittest: add nyc
    Signed-off-by: imnemo <291400733@qq.com>

--- test/test.syntax.js
+++ test/test.syntax.js
@@ -1,8 +1,8 @@
 var should = require('chai').should();
 var bufferFactory = require('../index');
 
-describe('Syntax', function(){
-  it('Should return a Buffer', function(){
+describe('Syntax', function () {
+  it('Should return a Buffer', function () {
     var buf = bufferFactory('2b', 'hex');
     (Buffer.isBuffer(buf)).should.equal(true);
   })
